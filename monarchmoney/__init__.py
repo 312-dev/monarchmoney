16bd1604de3e3798cde7aa9846eaa859456fee5e
Grayson Adams <51373669+GraysonCAdams@users.noreply.github.com>
2026-01-24 09:56:37 -0600

    chore: update maintainer to 312.dev with original author attribution
    - Add 312.dev as maintainer in __init__.py and setup.py
    - Keep hammem as original author with proper attribution
    - Add automatic version bump workflow on push to main
    
    [skip version]

--- monarchmoney/__init__.py
+++ monarchmoney/__init__.py
@@ -2,6 +2,9 @@
 monarchmoney
 
 A Python API for interacting with MonarchMoney.
+
+Originally created by hammem (https://github.com/hammem)
+Now maintained by 312.dev (https://github.com/312-dev)
 """
 
 from .monarchmoney import (
@@ -14,3 +17,4 @@ from .monarchmoney import (
 
 __version__ = "0.1.15"
 __author__ = "hammem"
+__maintainer__ = "312.dev"
